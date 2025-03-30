5b1a12f0e712da0dfa0e01d229a387eadcb9e53f
TuanAnh <tuanmeo980provip@gmail.com>
2025-03-31 01:07:18 +0700

    fix Home Page

--- Client/src/pages/home/HomePage.jsx
+++ Client/src/pages/home/HomePage.jsx
@@ -1,19 +1,11 @@
-import ScrollableCardList from './components/ScrollableCardList.jsx';
 import FeatureCategory from './components/FeatureCategory.jsx';
-import SportNews from './components/SportNews.jsx';
 import Banner from './components/Banner.jsx';
 
 const Home = () => {
   return (<div className="w-full text-center bg-gray-100 ">
         <div><Banner/></div>
         <div>
-          <ScrollableCardList/>
-        </div>
-        <div>
           <FeatureCategory/>
-        </div>
-        <div>
-          <SportNews/>
         </div>
       </div>);
 };
